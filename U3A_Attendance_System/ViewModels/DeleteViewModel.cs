4894f18ac33a125c9ac698a3bcbae50be642f4f8
Sebastian Kotyla <sebastian_kotyla@hotmail.com>
2013-06-13 21:07:17 +1000

    Some Session Validation

--- U3A_Attendance_System/ViewModels/DeleteViewModel.cs
+++ U3A_Attendance_System/ViewModels/DeleteViewModel.cs
@@ -51,65 +51,68 @@ namespace U3A_Attendance_System.ViewModels
         }
 
         public void Delete()
-        {            
-            if (itemToDelete is ICourseDescription)
+        {
+            if (itemToDelete != null)
             {
-                _facade.DeleteCourseDescription((itemToDelete as ICourseDescription).Id);
-            }
-
-            if (itemToDelete is IVenue)
-            {
-                IVenue venue = (IVenue)itemToDelete;
-
-                try
+                if (itemToDelete is ICourseDescription)
                 {
-                    _facade.DeleteVenue(venue.Id, venue.RegionId, venue.SuburbId);
-                }
-                catch (AssociationDependencyException e)
-                {
-                    settings.Title = "Delete Venue Locations";
-                    _wm.ShowDialog(new WarningViewModel((IVenue)venue), null, settings);                 
-                }
-            }
-
-            if (itemToDelete is ICoordinator)
-            {
-                _facade.DeleteCoordinator((itemToDelete as ICoordinator).Id);
-            }
-
-            if (itemToDelete is ICourseInstance)
-            {
-                ICourseInstance courseInstance = (ICourseInstance)itemToDelete;
-                var model = viewModel as CourseInstanceListViewModel;
-                
-                try
-                {
-                    _facade.DeleteCourseInstance(courseInstance.Id, courseInstance.RegionId);
-                    model.NotifyOfPropertyChange("InstancesList");
-                }
-                catch (AssociationDependencyException e)
-                {
-                    settings.Title = "Delete Instance Sessions";
-                    _wm.ShowDialog(new WarningViewModel((ICourseInstance)courseInstance), null, settings);
+                    _facade.DeleteCourseDescription((itemToDelete as ICourseDescription).Id);
                 }
 
-                
-            }
-
-            if (itemToDelete is ISession)
-            {
-                ISession session = (ISession)itemToDelete;
-
-                try
+                if (itemToDelete is IVenue)
                 {
-                    _facade.DeleteSession(session.Id, session.CourseInstanceId, session.CourseInstance.RegionId);
+                    IVenue venue = (IVenue)itemToDelete;
+
+                    try
+                    {
+                        _facade.DeleteVenue(venue.Id, venue.RegionId, venue.SuburbId);
+                    }
+                    catch (AssociationDependencyException e)
+                    {
+                        settings.Title = "Delete Venue Locations";
+                        _wm.ShowDialog(new WarningViewModel((IVenue)venue), null, settings);
+                    }
                 }
 
-                catch (AssociationDependencyException e)
+                if (itemToDelete is ICoordinator)
                 {
-                    settings.Title = "Delete Session Attendances";
-                    _wm.ShowDialog(new WarningViewModel((ISession)session), null, settings);
-                    NotifyOfPropertyChange("Sessions");
+                    _facade.DeleteCoordinator((itemToDelete as ICoordinator).Id);
+                }
+
+                if (itemToDelete is ICourseInstance)
+                {
+                    ICourseInstance courseInstance = (ICourseInstance)itemToDelete;
+                    var model = viewModel as CourseInstanceListViewModel;
+
+                    try
+                    {
+                        _facade.DeleteCourseInstance(courseInstance.Id, courseInstance.RegionId);
+                        model.NotifyOfPropertyChange("InstancesList");
+                    }
+                    catch (AssociationDependencyException e)
+                    {
+                        settings.Title = "Delete Instance Sessions";
+                        _wm.ShowDialog(new WarningViewModel((ICourseInstance)courseInstance), null, settings);
+                    }
+
+
+                }
+
+                if (itemToDelete is ISession)
+                {
+                    ISession session = (ISession)itemToDelete;
+
+                    try
+                    {
+                        _facade.DeleteSession(session.Id, session.CourseInstanceId, session.CourseInstance.RegionId);
+                    }
+
+                    catch (AssociationDependencyException e)
+                    {
+                        settings.Title = "Delete Session Attendances";
+                        _wm.ShowDialog(new WarningViewModel((ISession)session), null, settings);
+                        NotifyOfPropertyChange("Sessions");
+                    }
                 }
             }
         }
